1f7e616dcde178a328e29d88579e168d8aa246aa
2004667 <220103109@stu.sdu.edu.kz>
2024-05-25 21:52:39 +0500

    one of the projects

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+<h1 id="ss">Welcome to Almaty</h1>
+<h4 style="color:red">You are joke</h4>
+
+  
+</template>
+
+<style scoped>
+#ss{
+  display: flex;
+  justify-content: center;
+  align-items:center;
+}
+</style>
